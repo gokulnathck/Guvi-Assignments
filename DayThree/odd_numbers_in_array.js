3c12d385c2dfdf1b8f7a6de0e35d0cf25d913a53
Gokulnath CK <gokulnathck@Gokulnath-M2.local>
2023-08-10 04:32:27 +0530

    Add IIFE and arrow function for odd numbers

--- DayThree/odd_numbers_in_array.js
+++ DayThree/odd_numbers_in_array.js
@@ -1,5 +1,6 @@
-array_of_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let odd_numbers = function (numbers) {
+var array_of_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Anonymous function
+let odd_numbers_anonymous = function (numbers) {
     for (each_number in numbers){
         if (each_number % 2 !== 0) {
             console.log(each_number)
@@ -7,5 +8,24 @@ let odd_numbers = function (numbers) {
     }
 }
 
-odd_numbers(array_of_numbers)
+odd_numbers_anonymous(array_of_numbers);
 
+// Immediately invoked function expression
+(function odd_numbers_iife(){
+    for (each_number in array_of_numbers){
+        if (each_number % 2 !== 0) {
+            console.log(each_number)
+        }
+    }
+})();
+
+// Arrow function
+let odd_numbers_arrow = (numbers) => {
+    for (each_number in numbers){
+        if (each_number % 2 !== 0) {
+            console.log(each_number)
+        }
+    }
+}
+
+odd_numbers_arrow(array_of_numbers);
