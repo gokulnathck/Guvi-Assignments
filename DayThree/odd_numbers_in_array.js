1a08b805c46068faf6922c13b7cf97d936bf253b
Gokulnath CK <gokulnathck@Gokulnath-M2.local>
2023-08-10 04:34:32 +0530

    Add comments

--- DayThree/odd_numbers_in_array.js
+++ DayThree/odd_numbers_in_array.js
@@ -1,6 +1,7 @@
 var array_of_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 // Anonymous function
 let odd_numbers_anonymous = function (numbers) {
+    console.log('Anonymous Function')
     for (each_number in numbers){
         if (each_number % 2 !== 0) {
             console.log(each_number)
@@ -11,7 +12,8 @@ let odd_numbers_anonymous = function (numbers) {
 odd_numbers_anonymous(array_of_numbers);
 
 // Immediately invoked function expression
-(function odd_numbers_iife(){
+(function odd_numbers_iife() {
+    console.log('IIFE Function')
     for (each_number in array_of_numbers){
         if (each_number % 2 !== 0) {
             console.log(each_number)
@@ -21,6 +23,7 @@ odd_numbers_anonymous(array_of_numbers);
 
 // Arrow function
 let odd_numbers_arrow = (numbers) => {
+    console.log('Arrow Function')
     for (each_number in numbers){
         if (each_number % 2 !== 0) {
             console.log(each_number)
