48e2e6dc35e23a936fe6b1785e8d349f5b5b0040
Gokulnath CK <gokulnathck@Gokulnath-M2.local>
2023-12-09 03:10:57 +0530

    Add sum of all numbers

---
+++ DayThree/sum_of_all_numbers.js
@@ -0,0 +1,35 @@
+var array_of_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Anonymous function
+
+let odd_numbers_anonymous = function (numbers) {
+    console.log('-Anonymous Function-')
+    let sum = 0;
+    for (each_number of numbers) {
+        sum += each_number
+    }
+    console.log('Sum :', sum);
+}
+
+odd_numbers_anonymous(array_of_numbers);
+
+// Immediately invoked function expression
+(function odd_numbers_iife() {
+    console.log('-IIFE Function-')
+    let sum = 0;
+    for (each_number of array_of_numbers) {
+        sum += each_number
+    }
+    console.log('Sum :', sum);
+})();
+
+// Arrow function
+let odd_numbers_arrow = (numbers) => {
+    console.log('-Arrow Function-')
+    let sum = 0;
+    for (each_number of numbers) {
+        sum += each_number
+    }
+    console.log('Sum :', sum);
+}
+
+odd_numbers_arrow(array_of_numbers);
